88d5ddc814051ee4564407dcb04ace987dcc566e
adityagoel28 <adityagoel2002@gmail.com>
2022-05-14 23:47:49 +0530

    video upload done

--- vid2text/views.py
+++ vid2text/views.py
@@ -1,13 +1,11 @@
 from django.shortcuts import redirect, render
-
 from vid2text.models import videoUploader
-
+import requests
 
 # Create your views here.
 
 def home(request):
     return render(request, 'vid2text/index.html')
-
 
 def video(request):
     context = {}
@@ -15,27 +13,38 @@ def video(request):
 
 def upload(request):
     videoUpload = request.FILES['video']
-    videoUploader(video = videoUpload).save()
+    # videoUploader(video = videoUpload).save()
     print(videoUpload)
-    print(videoUpload.url)
+    # print(videoUpload.url)
+    file_obj = videoUploader.objects.create(video=videoUpload)
+    file_path = '/media/'+ str(file_obj.video)
+    print(file_path)
+    file_path = './' + file_path
+    print(file_path)
 
-    # jsondata = videoUploader.objects.filter(video = videoUpload)[0]
-    # print(jsondata)
-    # print(jsondata.path)
+    filename = file_path
+    def read_file(filename, chunk_size=5242880):
+        with open(filename, 'rb') as _file:
+            while True:
+                data = _file.read(chunk_size)
+                if not data:
+                    break
+                yield data
 
+    headers = {'authorization': "879ba2458d7c47debbc7189214746348"}
+    response = requests.post('https://api.assemblyai.com/v2/upload', headers=headers, data=read_file(filename))
 
-    # filename = "/path/to/foo.wav"
-    # def read_file(filename, chunk_size=5242880):
-    #     with open(filename, 'rb') as _file:
-    #         while True:
-    #             data = _file.read(chunk_size)
-    #             if not data:
-    #                 break
-    #             yield data
+    print(response.json()['upload_url'])
+    url = response.json()['upload_url']
 
-    # headers = {'authorization': "879ba2458d7c47debbc7189214746348"}
-    # response = requests.post('https://api.assemblyai.com/v2/upload', headers=headers, data=read_file(filename))
+    endpoint = "https://api.assemblyai.com/v2/transcript"
+    json = { "audio_url": url }
+    headers = {
+        "authorization": "879ba2458d7c47debbc7189214746348",
+        "content-type": "application/json"
+    }
+    response = requests.post(endpoint, json=json, headers=headers)
+    print(response.json())
 
-    # print(response.json())
     print('uploaded')
     return redirect(video)
